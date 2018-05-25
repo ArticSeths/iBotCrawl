32235ae6b6d6613175b1d931c3d81e198dcd7f88
Diego <diego.alfayate55@gmail.com>
2018-05-25 02:11:17 +0200

    pruebas

--- wp-content/themes/ibotcrawl/pages/prueba/page-prueba.php
+++ wp-content/themes/ibotcrawl/pages/prueba/page-prueba.php
@@ -18,7 +18,10 @@ function stylePage() {
     <meta charset="UTF-8">
     <title>Prueba</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
-                <script  src="https://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script  src="https://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.animate.js"></script>
 </head>
 <body>
     <input id="mensaje">
