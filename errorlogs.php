c02b1c51255698341044e448db63a6be19a7f826
Diego <diego.alfayate55@gmail.com>
2018-09-30 01:06:24 +0200

    error log

--- errorlogs.php
+++ errorlogs.php
@@ -1,4 +1,4 @@
 <?php
-$salida = shell_exec('tail -n 25 /data/wwwlog/www.ibotcrawl.com/ssl_error.log');
+$salida = shell_exec('tail -n 50 /data/wwwlog/www.ibotcrawl.com/ssl_error.log');
 echo "<pre>$salida</pre>";
 ?>
