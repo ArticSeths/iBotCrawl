f3d19656486c4e20dbc25e90553481fac2da62ba
Diego <diego.alfayate55@gmail.com>
2018-09-30 00:10:56 +0200

    renombrado rutas

--- wp-content/themes/ibotcrawl/pages/inicio/page-inicio.php
+++ wp-content/themes/ibotcrawl/pages/inicio/page-inicio.php
@@ -21,10 +21,10 @@
     <meta charset="utf-8" />
     <title>Inicio - iBotCrawl</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
-    <link rel="apple-touch-icon" href="/wp-content/themes/ibotcrawl/pages/ico/60.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/wp-content/themes/ibotcrawl/pages/ico/76.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/wp-content/themes/ibotcrawl/pages/ico/120.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/wp-content/themes/ibotcrawl/pages/ico/152.png">
+    <link rel="apple-touch-icon" href="/wp-content/themes/ibotcrawl/assets/ico/60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/wp-content/themes/ibotcrawl/assets/ico/76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/wp-content/themes/ibotcrawl/assets/ico/120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/wp-content/themes/ibotcrawl/assets/ico/152.png">
     <link rel="icon" type="image/x-icon" href="favicon.ico" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
@@ -37,14 +37,14 @@
     <link href="/wp-content/themes/ibotcrawl/assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css" media="screen" />
     <link href="/wp-content/themes/ibotcrawl/assets/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" media="screen" />
     <link href="/wp-content/themes/ibotcrawl/assets/plugins/switchery/css/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
-    <link href="/wp-content/themes/ibotcrawl/pages/css/pages-icons.css" rel="stylesheet" type="text/css">
-    <link class="main-stylesheet" href="/wp-content/themes/ibotcrawl/pages/css/themes/light.css" rel="stylesheet" type="text/css" />
+    <link href="/wp-content/themes/ibotcrawl/assets/css/pages-icons.css" rel="stylesheet" type="text/css">
+    <link class="main-stylesheet" href="/wp-content/themes/ibotcrawl/assets/css/themes/light.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript">
     window.onload = function()
     {
       // fix for windows 8
       if (navigator.appVersion.indexOf("Windows NT 6.2") != -1)
-        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/wp-content/themes/ibotcrawl/pages/css/windows.chrome.fix.css" />'
+        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/wp-content/themes/ibotcrawl/assets/css/windows.chrome.fix.css" />'
     }
     </script>
   </head>
@@ -131,7 +131,7 @@
     <script src="/wp-content/themes/ibotcrawl/assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
     <script src="/wp-content/themes/ibotcrawl/assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
     <!-- END VENDOR JS -->
-    <script src="/wp-content/themes/ibotcrawl/pages/js/pages.min.js"></script>
+    <script src="/wp-content/themes/ibotcrawl/assets/js/pages.min.js"></script>
   </body>
 </html>
 <?php
