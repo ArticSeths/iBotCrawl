987005e25ab8e797237d9f05b61377decf7fc405
Diego <diego.alfayate55@gmail.com>
2018-10-01 21:20:42 +0200

    test

--- wp-content/themes/ibotcrawl/functions.php
+++ wp-content/themes/ibotcrawl/functions.php
@@ -1,9 +1,11 @@
 <?php
 
 // requerimos todos los functions
-foreach(glob("functions/*.php") as $file){
-    error_log($file);
-    require_once($file);
+foreach (scandir(dirname(__FILE__)).'/functions/' as $filename) {
+    $path = dirname(__FILE__) . '/functions/' . $filename;
+    if (is_file($path)) {
+        require $path;
+    }
 }
 
 
